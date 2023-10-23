e8351dcd8877115611a4e8c595d988c61e3dcf58
구제서 <jsgu@koreanair.com>
2023-10-23 18:16:08 +0900

    commit

--- TFE/versions.tf
+++ TFE/versions.tf
@@ -1,12 +1,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-terraform {
-  required_version = ">= 0.15.1, < 2.0.0"
+# terraform {
+#   required_version = ">= 0.15.1, < 2.0.0"
 
-  required_providers {
-    tfe = {
-      version = "~> 0.49.2"
-    }
-  }
-}
+#   required_providers {
+#     tfe = {
+#       version = "~> 0.49.2"
+#     }
+#   }
+# }
