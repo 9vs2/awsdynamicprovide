28ce559db8dd7c7b9b84cae7778c44cafc706a6b
구제서 <jsgu@koreanair.com>
2023-10-23 18:21:31 +0900

    commit

--- TFE/variables.tf
+++ TFE/variables.tf
@@ -1,10 +1,22 @@
-variable "token" {
-  type = string
-  default = "CaDyv3gDuv3XPg.atlasv1.LnJ21aqOJmnvyUKcF1Y0OSZ7uzLTk3UyP5mpMJmKnCyzpQAzjD3LJrABSMymUxP485o"
-}
+# variable "token" {
+#   type = string
+#   default = "CaDyv3gDuv3XPg.atlasv1.LnJ21aqOJmnvyUKcF1Y0OSZ7uzLTk3UyP5mpMJmKnCyzpQAzjD3LJrABSMymUxP485o"
+# }
+
+# variable "hostname" {
+#   description = "hostname"
+#   default     = "app.terraform.io"
+#   type        = string
+# }
 
 variable "hostname" {
   description = "hostname"
   default     = "app.terraform.io"
+  type        = string
+}
+
+variable "token" {
+  description = "token"
+  default     = "WvpgoxeiCiNHQg.atlasv1.kxAERPGyVXi2VaUKJOcajVE7lFYZm3j5xFPAsdA5WIZJ215c1zEF545AdqxvEW5Mu2I"
   type        = string
 }
