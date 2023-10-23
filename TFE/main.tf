2e4477f412e3637ee28667bac7d2d42e6a2f9e2b
구제서 <jsgu@koreanair.com>
2023-10-23 18:13:46 +0900

    commit

--- TFE/main.tf
+++ TFE/main.tf
@@ -2,25 +2,63 @@
 #   source = "./main/User/KE/CDSO"
 # }
 
-data "tfe_organizations" "jsguawsct" {}
+provider "tfe" {
+  hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
+  token    = var.token
+  version  = "~> 0.49.2"
+}
 
-data "tfe_project" "default" {
+
+data "tfe_organizations" "foo" {}
+
+#resource "tfe_organization" "test-organization" {
+#  name  = "spartzames"
+#  email = "spartzames@gmail.com"
+#}
+
+data "tfe_project" "foo" {
   name = "awssso"
-  organization = data.tfe_organizations.jsguawsct.names[0]
+  organization = data.tfe_organizations.foo.names[0]
 }
 
-resource "tfe_workspace" "identityCenter" {
+resource "tfe_workspace" "test" {
   name         = "awsct_identityCenter"
-  organization = data.tfe_organizations.jsguawsct.names[0]
-  project_id = data.tfe_project.default.id
-  tag_names    = ["test", "sso"]
+  organization = data.tfe_organizations.foo.names[0]
+  project_id = data.tfe_project.foo.id
+  tag_names    = ["test", "app"]
+#  working_directory = "oidc"
+#  vcs_repo {
+#    identifier     = "kthong-aft/aft-account-customizations"
+#    oauth_token_id = "ot-T2D4n2cRf5igQjS8"
+#    branch = "master"
+#  }
 }
+
+## dynamic provider (INFRA-KALCDSO-DYNAMIC-PROVIDER)
+## 1개의 레파지토리, 3개의 디렉토리 (OIDC, IdentityCenterSet, SCPSet) -> 3개의 워크스페이스(TFC)
+
+
+
+
+# data "tfe_organizations" "jsguawsct" {}
+
+# data "tfe_project" "default" {
+#   name = "awssso"
+#   organization = data.tfe_organizations.jsguawsct.names[0]
+# }
+
+# resource "tfe_workspace" "identityCenter" {
+#   name         = "awsct_identityCenter"
+#   organization = data.tfe_organizations.jsguawsct.names[0]
+#   project_id = data.tfe_project.default.id
+#   tag_names    = ["test", "sso"]
+# }
 
 resource "tfe_variable" "dynamicProviderAuth" {
   key          = "TFC_AWS_PROVIDER_AUTH"
   value        = "true"
   category     = "env"
-  workspace_id = tfe_workspace.identityCenter.id
+  workspace_id = tfe_workspace.test.id
   description  = "a useful description"
 }
 
@@ -28,7 +66,7 @@ resource "tfe_variable" "dynamicProviderARN" {
   key          = "TFC_AWS_RUN_ROLE_ARN"
   value        = "abc"
   category     = "env"
-  workspace_id = tfe_workspace.identityCenter.id
+  workspace_id = tfe_workspace.test.id
   description  = "a useful description"
   sensitive    = true
 }
