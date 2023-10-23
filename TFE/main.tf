ab5e4c7ca793436c093a0be1dfd775ef45f2dbdb
구제서 <jsgu@koreanair.com>
2023-10-23 13:04:51 +0900

    commit

--- TFE/main.tf
+++ TFE/main.tf
@@ -6,9 +6,15 @@ data "tfe_organization" "awsct" {
   name = "jsguawsct"
 }
 
+data "tfe_project" "default" {
+  name = "Default Project"
+  organization = "jsguawsct"
+}
+
 resource "tfe_workspace" "identityCenter" {
   name         = "awsct_identityCenter"
   organization = tfe_organization.awsct.name
+  project_id = tfe_project.default.name
   tag_names    = ["test", "sso"]
 }
 
