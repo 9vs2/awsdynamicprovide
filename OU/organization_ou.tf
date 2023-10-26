ad5c81a80cfe19138987ba2b665a870f67dd1bbc
구제서 <jsgu@koreanair.com>
2023-10-26 10:42:46 +0900

    commit

--- OU/organization_ou.tf
+++ OU/organization_ou.tf
@@ -2,6 +2,6 @@ data "aws_organizations_organization" "organization" {}
 
 resource "aws_organizations_organizational_unit" "organization_unit" {
   name      = "Core"
-  parent_id = data.aws_organizations_organization.example.roots[0].id
+  parent_id = data.aws_organizations_organization.organization.roots[0].id
 
 }
